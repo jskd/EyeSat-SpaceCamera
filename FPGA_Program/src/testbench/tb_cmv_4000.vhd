7089a01b5cb4b4fd75ce275b107939284c5d4269
Jérôme Skoda <jerome.skoda@hotmail.fr>
2016-04-09 00:56:21 +0200

    update tb_cmv

--- FPGA_Program/src/testbench/tb_cmv_4000.vhd
+++ FPGA_Program/src/testbench/tb_cmv_4000.vhd
@@ -1,30 +1,31 @@
 library ieee;
 use ieee.std_logic_1164.all;
 use std.textio.all;
-use work.txt_util.all;
-														
-														
+use work.hex_to_std_logic_vector.all;
+		
+												
 entity tb_cmv_4000 is
 	port
 	(
 		--Tana:				out	std_logic;
 		SYS_RES_N:	in	std_logic := '1';
-		CLK_IN: 		in 	std_logic;
-		FRAME_REQ:	in	std_logic;
+		--CLK_IN: 		in 	std_logic;
+		FRAME_REQ:	in	std_logic := '1';
 		--T_EXP2:			in	std_logic;
 		--T_EXP1: 		in	std_logic;
-		SPI_EN: 		in	std_logic;
-		SPI_CLK:		in	std_logic;
-		SPI_IN:			in	std_logic;
-		SPI_OUT:		out	std_logic;
+		--SPI_EN: 		in	std_logic;
+		--SPI_CLK:		in	std_logic;
+		--SPI_IN:			in	std_logic;
+		--SPI_OUT:		out	std_logic;
 		--TDIG2:			out	std_logic;
 		--TDIG1:			out	std_logic;
 		--LVDS_CLK:		in	std_logic;
-		OUTCLK:			out	std_logic;
-		LVDS_OUTCTR:			out	std_logic;
+		OUTCLK:					out	std_logic;
+		LVDS_OUTCTR:		out	std_logic;
 		LVDS_OUTDATA:		out	std_logic_vector(15 downto 0)
 	);
 end entity;
+
 
 architecture tb_cmv_4000_arch of tb_cmv_4000 is
   -- Procedure for clock generation
@@ -45,86 +46,112 @@ architecture tb_cmv_4000_arch of tb_cmv_4000 is
     end loop;
   end procedure;
 
-	file f_data: text; 
+	
+	
+	
+	file f_data: text open read_mode is "..\src\testbench\pixel_ouput.data"; 
 
-	signal clk : std_logic := '0';
-	signal clk_gen_lvds : std_logic := '0';
-	signal	clk_lvds: std_logic := '0';
 	
-	signal next_lvds_digit : integer range 0 to 10 := 0;
+	signal lvds_digit : integer range 0 to 10 := 0;
 	
-	signal current_lvds_digit: integer range 0 to 10 := 0;
 	
 
 	type state_t is (waiting_req, init, px_burst, burst_oh, row_oh);
-	signal curent_s: state_t := init;
+	signal curent_state: state_t := waiting_req;
+	signal next_state: state_t := waiting_req;
 	
-	type YOUR_ARRAY_TYPE is array (0 to 15) of std_logic_vector(9 downto 0);
-	signal buf_LVDS_OUTDATA : YOUR_ARRAY_TYPE;
 	
-	signal buf_LVDS_OUTCTR : std_logic_vector(9 downto 0);
-	signal buf_curent_s : std_logic_vector(3 downto 0);
+	subtype  one_channel_data_output_buf_t is std_logic_vector(11 downto 0);
+	type all_channel_data_output_buf_t is array (0 to 15) of one_channel_data_output_buf_t;
+	signal current_channel_data_v	: all_channel_data_output_buf_t;
+	signal next_channel_data_v		: all_channel_data_output_buf_t;
 	
-	signal buf : std_logic_vector(173 downto 0);
+	signal current_channel_ctr_v	: one_channel_data_output_buf_t;
+	signal next_channel_ctr_v			: one_channel_data_output_buf_t;
+	
+
+		
+	signal clk_gen_lvds : std_logic := '0';
+	signal clk : std_logic := '0';
+			
 begin
 
   clk_gen(clk_gen_lvds, 50.000E6);
-	clk_lvds <= SYS_RES_N and clk_gen_lvds;
-	OUTCLK <= clk_lvds;
-
-	process(clk_lvds)
 	
-		variable f_line: line;		
-		variable f_str: string(1 to 174);
+	clk <= SYS_RES_N and clk_gen_lvds;
+	OUTCLK <= clk;
+	
+	LVDS_OUTCTR <= current_channel_ctr_v(lvds_digit);
+	data: for i in 0 to 15 generate
+		LVDS_OUTDATA(i) <= current_channel_data_v(i)(lvds_digit);
+	end generate data;
+
+	update_state: process(clk) 
+		begin
+		if lvds_digit = 9 then		
+			curent_state <= next_state;
+			current_channel_ctr_v <= next_channel_ctr_v;
+			for i in 0 to 15 loop
+				current_channel_data_v(i) <= next_channel_data_v(i);
+			end loop;
+		end if;
+	end process update_state;
+	
+	reader_file: process(clk)
+	
+		variable f_line		: line;		
+		variable f_str		: string(1 to  52);
+		variable line_hex : std_logic_vector( 207 downto 0);
 		
 		begin
 		
-			current_lvds_digit <= next_lvds_digit;
+		if lvds_digit = 0 then	
 		
-			-- update outdata
-			for i in 0 to 15 loop
-				LVDS_OUTDATA(i) <= buf_LVDS_OUTDATA(i)(next_lvds_digit);
-			end loop;
+			if curent_state = waiting_req or endfile(f_data) then	
 			
-			-- update ctr
-			LVDS_OUTCTR <= buf_LVDS_OUTCTR(next_lvds_digit);
+				if FRAME_REQ = '1' then 
+					next_state <= init;
+				else
+					next_state <= waiting_req;
+				end if;
 				
-			-- update state
-			case buf_curent_s is
-				when "0000" => curent_s <= px_burst;
-				when "0001" => curent_s <= burst_oh;
-				when "0010" => curent_s <= row_oh;
-				when others => curent_s <= init;
-			end case;
-
-			-- read file
-			if (next_lvds_digit = 7) and (curent_s = init) then	
-				file_open(f_data, "..\src\testbench\pixel_ouput.data", read_mode);
-			end if;
-
-			-- read line of file
-			if (next_lvds_digit = 8) and (curent_s /= waiting_req) then
-				readline(f_data, f_line);
-				read(f_line, f_str);
-				buf <= to_std_logic_vector(f_str);
-			end if;
-
-			-- update ouput buffer
-			if (next_lvds_digit = 9) and (curent_s /= waiting_req) then
-				buf_curent_s <= buf(buf'LEFT  downto buf'LEFT - 3);
-				buf_LVDS_OUTCTR <= buf(buf'LEFT - 4 downto buf'LEFT - 13);
+				next_channel_ctr_v <= "001000000000";
 				for i in 0 to 15 loop
-					buf_LVDS_OUTDATA(i) <= buf(buf'LEFT - 14 - i*10 downto buf'LEFT - 23 - i*10);
+					next_channel_data_v(i) <= (others => '0');
 				end loop;
-			end if;
+				
+			else 
+				
+					readline(f_data, f_line);
+					read(f_line, f_str);
+					line_hex := hexstring_to_std_logic_vector(f_str);
+					
+					case line_hex(line_hex'LEFT downto line_hex'LEFT - 3) is
+						when "0001" => next_state <= px_burst;
+						when "0010" => next_state <= burst_oh;
+						when "0011" => next_state <= row_oh;
+						when others => next_state <= curent_state;
+					end case;
+					
+					next_channel_ctr_v <= line_hex( line_hex'LEFT - 4  downto line_hex'LEFT - 4 - 12 + 1);
 
-			-- update conter
-			if next_lvds_digit = 9 then
-				next_lvds_digit <= 0;
-			else
-				next_lvds_digit <= next_lvds_digit+1;
-			end if;
+					for i in 0 to 15 loop
+						next_channel_data_v(i) <= line_hex(line_hex'LEFT - 4 - (i+1)*12 downto line_hex'LEFT - 4 - 12 + 1 - (i+1)*12  );
+					end loop;
 
-	end process;
+			end if;
+			
+		end if;
+
+	end process reader_file;
+		
+	digit_increment: process(clk)		
+		begin	
+		if lvds_digit = 9 then
+			lvds_digit <= 0;
+		else
+			lvds_digit <= lvds_digit+1;
+		end if;
+	end process digit_increment;
 
 end architecture;
