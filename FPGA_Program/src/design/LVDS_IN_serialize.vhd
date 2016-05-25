e53d40cc7eed080e1f692ccdf25e09536bc81a66
innovlab <innovlab>
2016-05-25 16:36:39 +0200

    init quartus project

--- FPGA_Program/src/design/LVDS_IN_serialize.vhd
+++ FPGA_Program/src/design/LVDS_IN_serialize.vhd
@@ -5,9 +5,10 @@ entity lvds_in_serialize is
 
   port(
     LVDS_CLK : in std_logic;
+    LVDS_CTR : in std_logic;
     LVDS_CH1 : in std_logic;
     LVDS_CH9 : in std_logic;
-    LVDS_DATA: out std_logic_vector(2 downto 0)
+    LVDS_DATA: out std_logic_vector(3 downto 0)
 	);
 
 end entity lvds_in_serialize;
@@ -15,8 +16,6 @@ end entity lvds_in_serialize;
 architecture lvds_in_serialize_a of lvds_in_serialize is
 
 	begin  
-		LVDS_DATA <=	LVDS_CLK &
-									LVDS_CH1 &
-									LVDS_CH9;
+		LVDS_DATA <= LVDS_CTR & LVDS_CLK & LVDS_CH1 & LVDS_CH9;
 
 end architecture lvds_in_serialize_a;
