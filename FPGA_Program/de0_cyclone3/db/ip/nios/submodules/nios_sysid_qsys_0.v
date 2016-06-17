69ccceff1dca278cf6d42446147d2947dc914269
jskd <jerome.skoda@hotmail.fr>
2016-06-17 18:55:34 +0200

    read/write reg of cmv4000

--- FPGA_Program/de0_cyclone3/db/ip/nios/submodules/nios_sysid_qsys_0.v
+++ FPGA_Program/de0_cyclone3/db/ip/nios/submodules/nios_sysid_qsys_0.v
@@ -36,7 +36,7 @@ module nios_sysid_qsys_0 (
 
   wire    [ 31: 0] readdata;
   //control_slave, which is an e_avalon_slave
-  assign readdata = address ? 1466173370 : 3768044508;
+  assign readdata = address ? 1466180753 : 3768044508;
 
 endmodule
 
--- FPGA_Program/de0_cyclone3/nios/software/cmv_controller/main.cpp
+++ FPGA_Program/de0_cyclone3/nios/software/cmv_controller/main.cpp
@@ -1,19 +1,39 @@
 #include "system.h"
 #include "controller/UART.h"
+#include "controller/SPI.h"
 
 int main() {
 
-	UART* test = new UART(UART_BASE, UART_IRQ, 64, 64);
+	UART* uart = new UART(UART_BASE, UART_IRQ, 64, 64);
+
+	SPI* spi= new SPI(SPI_BASE);
+
+
 
 	while (1) {
-		if (!test->inBuffer_isEmpty()) {
-			char c = test->buffer_getchar();
-			test->buffer_printf("caractere %c %d %c\n", c, 12, c);
+		if (!uart->inBuffer_isEmpty()) {
+			char c = uart->buffer_getchar();
+			unsigned char value;
+
+
+			value = spi->readCMVRegister(72);
+
+
+			uart->buffer_printf("Avant reg(72)=%d ,", value);
+
+
+			bool suc = spi->writeCMVRegister(72, 3);
+
+			uart->buffer_printf("%s", suc==true?"ok":"fail");
+
+			value = spi->readCMVRegister(72);
+
+			uart->buffer_printf("Apres reg(72)=%d \r\n", value);
+
+
+
 		}
-
-
-
-		test->send();
+		uart->send();
 	}
 	return 0;
 }
